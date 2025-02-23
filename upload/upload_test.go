e86a31b6b3114dc8170af38c26d6e6d4b1590dfd
fengxiu1986 <123361334@qq.com>
2025-02-23 13:31:00 +0800

    Create upload_test.go

---
+++ upload/upload_test.go
@@ -0,0 +1,17 @@
+package upload
+
+import (
+	"testing"
+)
+
+func TestUploadChecked(t *testing.T) {
+
+	// ui, err := NewUploadImageByUncheckImage("/gameicon/[uncheck]6cc701e3-bb94-42db-ac9b-69e657c1ea4c.jpeg")
+	// if err != nil {
+	// 	t.Error(err)
+	// }
+	// err = ui.Checked()
+	// if err != nil {
+	// 	t.Error(err)
+	// }
+}
